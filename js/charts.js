1355b536836e8c736390be2db7c3c8b9d202a64b
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-08 20:33:27 +0200

    functionality for displaying charts

--- js/charts.js
+++ js/charts.js
@@ -2,165 +2,175 @@
 // CHARTS
 //---------
 
-let sum = 0;
-let empiric_data = [];
-[...uniqueFrequencies].forEach(val => {
-    sum += val / numbers.length;
-    empiric_data.push(sum);
-})
+function buildGraphs(){
 
-sum = 0;
-let cumulative_data = [];
-[...uniqueFrequencies].forEach(val => {
-    sum += val;
-    cumulative_data.push(sum);
-})
+    let text = [];
+    let text_cum = [];
 
-let rel_poli_data = [];
-[...uniqueFrequencies].forEach(val => {
-    rel_poli_data.push(val / numbers.length);
-})
+    let gist_data = [];
+    let gist_rel_data = [];
 
-let text = [];
-for (i = 0; i < uniqueNums.length; i++) {
-    if (i == 0)
-        text.push(`${uniqueNums[0]}-${uniqueNums[1]}`);
-    else if (i == uniqueNums.length - 1)
-        text.push(`${uniqueNums[uniqueNums.length - 1]}-infinity`);
-    else
-        text.push(`${uniqueNums[i]}-${uniqueNums[i + 1]}`);
+    let cum_data = [];
+    let cum_rel_data = [];
+
+    let tempSum = 0;
+    for(let [key, value] of intervalToFrequency){
+        tempSum += value;
+
+        text.push(`${key[0].toFixed(2)}-${key[1].toFixed(2)}`);
+        text_cum.push(`${((key[0]+key[1])/2).toFixed(2)}`);
+        gist_data.push(value);
+        gist_rel_data.push(value/numbers.length);
+        cum_data.push(tempSum);
+        cum_rel_data.push(tempSum/numbers.length);
+    }
+    
+
+
+    //gistogram function
+    var gist = document.getElementById('gisto').getContext('2d');
+    var chart = new Chart(gist, {
+        type: 'bar',
+        data: {
+            labels: text,
+            datasets: [{
+                label: 'Гістограма частот',
+                borderWidth: 2,
+                borderColor: 'black',
+                borderWidth:{ top:3, right:3, bottom:0, left:3 },
+                barPercentage: 0.5,
+                barThickness: 110,
+                maxBarThickness: 180,
+                minBarLength: 2,
+                data: gist_data
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false,
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    
+    //gistogram relative function
+    var gist_rel = document.getElementById('gisto_rel').getContext('2d');
+    var chart = new Chart(gist_rel, {
+        type: 'bar',
+        data: {
+            labels: text,
+            datasets: [{
+                label: 'Гістограма відносних частот',
+                borderWidth: 2,
+                borderColor: 'black',
+                borderWidth:{ top:3, right:3, bottom:0, left:3 },
+                barPercentage: 0.5,
+                barThickness: 110,
+                maxBarThickness: 180,
+                minBarLength: 2,
+                data: gist_rel_data
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false,
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
+    //cumulative curve
+    var cum = document.getElementById('cumulative').getContext('2d');
+    var chart = new Chart(cum, {
+        type: 'line',
+        data: {
+            labels: text_cum,
+            datasets: [{
+                label: 'Кумулятивна крива',
+                borderColor: 'black',
+                data: cum_data
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    //relative cumulative curve
+    var cum = document.getElementById('cumulative_rel').getContext('2d');
+    var chart = new Chart(cum, {
+        type: 'line',
+        data: {
+            labels: text_cum,
+            datasets: [{
+                label: 'Кумулятивна крива (за відносними частотами)',
+                borderColor: 'black',
+                data: cum_rel_data
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
+    //empirical function
+    var emp = document.getElementById('empirical').getContext('2d');
+    var chart = new Chart(emp, {
+        type: 'bar',
+        data: {
+            labels: text,
+            datasets: [{
+                label: 'Емпірична функція розподілу',
+                borderWidth: 2,
+                borderColor: 'black',
+                borderWidth:{ top:3, right:0, bottom:0, left:0 },
+                barPercentage: 0.5,
+                barThickness: 110,
+                maxBarThickness: 180,
+                minBarLength: 2,
+                data: cum_rel_data
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false,
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
+    
+    
+    
 }
-
-
-//poligon
-var poli = document.getElementById('poligon').getContext('2d');
-var chart = new Chart(poli, {
-    type: 'line',
-    data: {
-        labels: uniqueNums,
-        datasets: [{
-            label: 'Полігон частот',
-            borderColor: 'black',
-            data: uniqueFrequencies,
-            lineTension: 0
-        }],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-//poligon rel
-var poli_rel = document.getElementById('poligon_rel').getContext('2d');
-var chart = new Chart(poli_rel, {
-    type: 'line',
-    data: {
-        labels: uniqueNums,
-        datasets: [{
-            label: 'Полігон частот (за відносними частотами)',
-            borderColor: 'black',
-            data: rel_poli_data,
-            lineTension: 0
-        }],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-//cumulative curve
-var cum = document.getElementById('cumulative').getContext('2d');
-var chart = new Chart(cum, {
-    type: 'line',
-    data: {
-        labels: uniqueNums,
-        datasets: [{
-            label: 'Кумулятивна крива',
-            borderColor: 'black',
-            data: cumulative_data
-        }],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-//cumulative curve rel
-var cum_rel = document.getElementById('cumulative_rel').getContext('2d');
-var chart = new Chart(cum_rel, {
-    type: 'line',
-    data: {
-        labels: uniqueNums,
-        datasets: [{
-            label: 'Кумулятивна крива (за відносними частотами)',
-            borderColor: 'black',
-            data: empiric_data
-        }],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-//empirical function
-var emp = document.getElementById('empirical').getContext('2d');
-var chart = new Chart(emp, {
-    type: 'bar',
-    data: {
-        labels: text,
-        datasets: [{
-            label: 'Емпірична функція розподілу',
-            borderWidth: 2,
-            borderColor: 'black',
-            borderWidth:{ top:3, right:0, bottom:0, left:0 },
-            barPercentage: 0.5,
-            barThickness: 75,
-            maxBarThickness: 180,
-            minBarLength: 2,
-            data: empiric_data
-        }],
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                gridLines: {
-                    drawBorder: false,
-                },
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-
