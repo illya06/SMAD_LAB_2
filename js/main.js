dd53876ff4b546063e98bd9a33c3261eae38d770
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-08 17:18:05 +0200

    main function

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function main() {
+
+    //trend
+    calcTrend();
+
+    //median
+    calcMedian();
+
+    //span
+    calcSpan();
+
+    // mid Statistical
+    calcMidStatistical();
+
+    //dispersion
+    calcDispersion();
+
+    //standardDeviation
+    calcStandardDeviation();
+
+    //correctedStandardDeviation
+    calcCorrectedDispersion();
+
+    //correctedStandardDeviation
+    calcCorrectedStandardDeviation();
+
+    //variation
+    calcVariation();
+
+    //calcInitialMoment
+    calcInitialMomentOfK(3);
+
+    //centralMoment
+    calcCentralMomentOfK(3);
+
+    //asymmetry
+    calcAsymmetry();
+
+    //excess
+    calcExcess();
+}
