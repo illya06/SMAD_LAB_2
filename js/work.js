22c4c5e2810788d547aa016d4fdd25b671bef8dd
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-08 19:50:38 +0200

    for parameter calculation

--- js/work.js
+++ js/work.js
@@ -29,16 +29,11 @@ for (var i = minVal; i < len * r; i += len) {
     var ammountInInterval = 0;
     numbers.forEach(num => {
         if (i != len * r - minVal) {
-            if (num >= i && num < i + len) {
-                //alert(`${num} belongs to : ${i.toFixed(4)} - ${(i + len).toFixed(4)}`);
+            if (num >= i && num < i + len)
                 ammountInInterval += 1;
-
-            }
         } else {
-            if (num >= i && num <= i + len){
+            if (num >= i && num <= i + len)
                 ammountInInterval += 1;
-                //alert(`${num} belongs to : ${i.toFixed(4)} - ${(i + len).toFixed(4)}`);
-            }
         }
     })
     intervalToFrequency.set([i, i + len], ammountInInterval);
@@ -51,22 +46,22 @@ var stdDeviation = 0;
 
 function calcExcess() {
     document.getElementById('excess').innerHTML =
-        ` <kbd>${calcCentralMomentOfK(4, false) / calcInitialMomentOfK(4, false) - 3}</kbd>`;
+        ` <kbd>${(calcCentralMomentOfK(4, false) / calcInitialMomentOfK(4, false) - 3).toFixed(3)}</kbd>`;
 }
 
 function calcAsymmetry() {
     document.getElementById('asymmetry').innerHTML =
-        ` <kbd>${calcCentralMomentOfK(3, false) / calcInitialMomentOfK(3, false)}</kbd>`;
+        ` <kbd>${(calcCentralMomentOfK(3, false) / calcInitialMomentOfK(3, false)).toFixed(3)}</kbd>`;
 }
 
 function calcCentralMomentOfK(k, print = true) {
     var centralMomentOfK = 0;
     intervalToFrequency.forEach((frequency, interv) => {
-        centralMomentOfK += (Math.pow(((interv[0] - interv[1]) / 2) - midStat, k) * frequency) / numbers.length;
+        centralMomentOfK += (Math.pow(((interv[0] + interv[1]) / 2) - midStat, k) * frequency) / numbers.length;
     });
 
     if (print) {
-        document.getElementById('centralMoment').innerHTML = ` <kbd>${centralMomentOfK}</kbd>`;
+        document.getElementById('centralMoment').innerHTML = ` <kbd>${centralMomentOfK.toFixed(3)}</kbd>`;
     }
     return centralMomentOfK;
 }
@@ -75,65 +70,89 @@ function calcCentralMomentOfK(k, print = true) {
 function calcInitialMomentOfK(k, print = true) {
     var initialMomentOfK = 0;
     intervalToFrequency.forEach((frequency, interv) => {
-        initialMomentOfK += (Math.pow(((interv[0] - interv[1]) / 2), k) * frequency) / numbers.length;
+        initialMomentOfK += (Math.pow(((interv[0] + interv[1]) / 2), k) * frequency) / numbers.length;
     });
 
     if (print) {
-        document.getElementById('initialMoment').innerHTML = ` <kbd>${initialMomentOfK}</kbd>`;
+        document.getElementById('initialMoment').innerHTML = ` <kbd>${initialMomentOfK.toFixed(3)}</kbd>`;
     }
     return initialMomentOfK;
 }
 
 function calcVariation() {
     document.getElementById('variation').innerHTML =
-        ` <kbd>${stdDeviation / midStat}</kbd>`;
+        ` <kbd>${(stdDeviation / midStat).toFixed(3)}</kbd>`;
 }
 
 function calcCorrectedStandardDeviation() {
     document.getElementById('correctedStandardDeviation').innerHTML =
-        ` <kbd>${stdDeviation * Math.sqrt((numbers.length / (numbers.length - 1)))}</kbd>`;
+        ` <kbd>${(stdDeviation * Math.sqrt((numbers.length / (numbers.length - 1)))).toFixed(3)}</kbd>`;
 }
 
 function calcCorrectedDispersion() {
     document.getElementById('correctedDispersion').innerHTML =
-        ` <kbd>${dispersion * (numbers.length / (numbers.length - 1))}</kbd>`;
+        ` <kbd>${(dispersion * (numbers.length / (numbers.length - 1))).toFixed(3)}</kbd>`;
 }
 
 function calcStandardDeviation() {
     stdDeviation = Math.sqrt(dispersion);
     document.getElementById('standardDeviation').innerHTML =
-        ` <kbd>${stdDeviation}</kbd>`;
+        ` <kbd>${stdDeviation.toFixed(3)}</kbd>`;
 }
 
 function calcMidStatistical() {
     intervalToFrequency.forEach((frequency, interv) => {
-        midStat += (((interv[0] - interv[1]) / 2) * frequency) / numbers.length;
+        midStat += (((interv[0] + interv[1]) / 2) * frequency) / numbers.length;
     });
-    document.getElementById('midStatistical').innerHTML = ` <kbd>${midStat}</kbd>`;
+    document.getElementById('midStatistical').innerHTML = ` <kbd>${midStat.toFixed(3)}</kbd>`;
 }
 
 function calcDispersion() {
     intervalToFrequency.forEach((frequency, interv) => {
-        dispersion += (Math.pow(((interv[0] - interv[1]) / 2) - midStat, 2) * frequency) / numbers.length;
+        dispersion += (Math.pow(((interv[0] + interv[1]) / 2) - midStat, 2) * frequency) / numbers.length;
     });
-    document.getElementById('dispersion').innerHTML = ` <kbd>${dispersion}</kbd>`;
+    document.getElementById('dispersion').innerHTML = ` <kbd>${dispersion.toFixed(3)}</kbd>`;
 }
 
 function calcTrend() {
-    var max = 0;
+    var index = 0;
     var trend = 0;
-    intervalToFrequency.forEach((apearence, number) => {
-        if (apearence > max) {
-            max = apearence;
-            trend = number;
+    let a = Array.from(intervalToFrequency, ([key, value]) => ({ key, value }));
+
+    let i = 0, max = 0;
+    for (let [_, value] of intervalToFrequency) {
+        if (value >= max){
+            index = i;
+            max = value;
         }
-    });
-    document.getElementById('trend').innerHTML = ` <kbd>${trend}</kbd>`;
+        i++;
+    }
+    
+    trend = a[index]['key'][0] + len*(a[index]['value'] - a[index-1]['value'])/(2*a[index]['value'] - a[index-1]['value']- a[index+1]['value']);
+    document.getElementById('trend').innerHTML = ` <kbd>${trend.toFixed(3)}</kbd>`;
+    //document.getElementById('trend').innerHTML = ` <kbd>FIX PLS</kbd>`;
 }
 
 function calcMedian() {
+    let a = Array.from(intervalToFrequency, ([key, value]) => ({ key, value }));
+   
+    let sum = 0;
+    for (let [_, value] of intervalToFrequency) {
+        sum += value;
+    }
+
+    let kumSum = 0, i = 0;
+    for (let [_, value] of intervalToFrequency) {
+        kumSum += value;
+        if (kumSum >= sum/2)
+            break;
+        i++;
+    }
+
+    let median = a[i]['key'][0] + (len/a[i]['value'])*(sum/2 - kumSum + a[i]['value']);
+
     document.getElementById('median').innerHTML =
-        ` <kbd> DUNNO </kbd>`;
+        ` <kbd> ${median.toFixed(3)} </kbd>`;
 }
 
 function calcSpan() {
